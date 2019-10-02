972fd0ff04cfee9f6b26c67fc447fb00caf97e91
unitech1120 <uni.tech1120@gmail.com>
2019-10-02 18:14:28 +0100

    Add Max Title length

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,17 +1,17 @@
 <template>
 <div>
-    <div class="tile is-parent">
-        
+    <router-link :to="{name: 'details', params:{project: project}}" tag="div">   
+        <div class="tile is-parent">
             <article class="tile is-child notification is-info">
-                <router-link :to="{name: 'details', params:{project: project}}" tag="p">{{project.title}} </router-link>
+                <h1 class="title" v-if="project.title.length < 21">{{project.title}}</h1>
+                <h1 class="title" v-else>{{project.title.substring(0,18)+"..."}}</h1> 
                 <p class="subtitle">With an image</p>
                 <figure class="image is-4by3">
                 <img src="https://images.unsplash.com/photo-1569817480337-01a8b22cd8d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80">
                 </figure>
             </article>
-       
-    </div>
-
+        </div>
+    </router-link>
     <!-- <div  v-bind:class="{'is-complete':project.completed}" >
         <input type="checkbox" v-on:change="markComplete">
         <div class="title">{{project.title}}</div>
@@ -38,6 +38,11 @@ export default {
 </script>
 
 <style scoped>
+    /* .tile{
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    } */
 
     .project-item { 
         background: #f4f4f4;
