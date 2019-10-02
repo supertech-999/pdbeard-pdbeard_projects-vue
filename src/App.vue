d9503255fc4d70af5debef1215525b9ecb7a27f6
unitech1120 <uni.tech1120@gmail.com>
2019-10-02 18:00:12 +0100

    Basic transition effects

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <div id="nav">
       <Header />
     </div> 
-    <router-view/>
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+      <router-view/>
+    </transition>
  </div>
 </template>
 
@@ -19,7 +24,17 @@ export default {
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
 
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
 /* * {
   box-sizing: border-box;
   margin: 0;
